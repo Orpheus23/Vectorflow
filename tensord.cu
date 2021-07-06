b9c48cf02be616b93fefc3bf54205b76fc2c0524
Orpheus23 <you@example.com>
2021-07-06 16:46:47 +0530

    concat along axis complete

--- tensord.cu
+++ tensord.cu
@@ -407,25 +407,33 @@ class Tensor
 
         //concats two vectors along a given axis
         void concat (Tensor &b, int axis)
-        {   long long new_shape = (shape_total/dimension_list[axis])*(stride_vector[axis]+b.shape()[axis]);
-            long long sub_index;
+        {   long long new_shape = (shape_total/dimension_list[axis])*(dimension_list[axis]+b.shape()[axis]);
+            int orig_dim = dimension_list[axis];
+            long long sub_index = 0;
             dimension_list[axis] += b.shape()[axis];
+            
+            int opp_idx = 0;
             vector<T> output_tensor(new_shape,0);
             for (long long i = 0; i< new_shape;i++)
             {
                 if (((i/stride_vector[axis])%dimension_list[axis] ) < orig_dim )
                 {
-                    output_tensor[i] = tensor_cpu[i];
-                    sub_index = (long long)i;
+                    output_tensor[i] = tensor_cpu[i-opp_idx];
+                    cout<<(i/stride_vector[axis])%dimension_list[axis]<<",";
+                    sub_index+=1;
                 }
                 else
                 {
-                    output_tensor[i] = b[i-sub_index-1];
+                    cout << i-sub_index<<" ,";
+                    output_tensor[i] = b[i-sub_index];
+                    opp_idx +=1;
+                    
                 }
                
             }
-            tensor_cpu = output_tensor;
+            cout <<endl;
             stride_vector = stride_convert(dimension_list);
+            tensor_cpu = output_tensor;
 
 
         }
