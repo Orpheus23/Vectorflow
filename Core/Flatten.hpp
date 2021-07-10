208868de4114966693713b9b11fb2e831c808fe5
Orpheus23 <you@example.com>
2021-07-10 18:48:23 +0530

    namespace-02

--- Core/Flatten.hpp
+++ Core/Flatten.hpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 template <typename T>
-std::vector<T> Flatten_with_dims(vector<T> v,vector<T> empty,vector<long long int> &dims,int level)
+std::vector<T> Flatten_with_dims(std::vector<T> v,std::vector<T> empty,std::vector<long long int> &dims,int level)
 {
-    vector <T> temp;
+    std::vector <T> temp;
     temp.insert(temp.end(),v.begin(),v.end());
     if (dims.size()==level) 
     {
@@ -15,14 +15,14 @@ std::vector<T> Flatten_with_dims(vector<T> v,vector<T> empty,vector<long long in
 
 
 template<class T,typename T_>
-std::vector<T_> Flatten_with_dims(vector<T> &rows,vector<T_> empty,vector<long long int> &dims,int &level)
+std::vector<T_> Flatten_with_dims(std::vector<T> &rows,std::vector<T_> empty,std::vector<long long int> &dims,int &level)
 {
     
     if (dims.size()==level) 
     {
         dims.push_back(rows.size());
     }
-    vector <T_> empty_temp ;
+    std::vector <T_> empty_temp ;
     for (T element:rows)
     {   
         empty_temp = Flatten_with_dims(element,empty,dims,level+1);
